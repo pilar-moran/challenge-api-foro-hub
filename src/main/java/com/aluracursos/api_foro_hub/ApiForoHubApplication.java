1f02afc07f0b6c6aad5af47ff1edf475b8cc5fe5
Pilar Morán <pmoranf.cl@gmail.com>
2025-08-15 15:24:21 -0400

    feat: initial commit

---
+++ src/main/java/com/aluracursos/api_foro_hub/ApiForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.api_foro_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForoHubApplication.class, args);
+	}
+
+}
